1a804f9f56458592509ca7c997e0d2f91f28d014
Adithyasy105 <geethageetha2135@gmail.com>
2025-11-01 21:50:47 +0530

    Add API files with .js extensions for Vercel

--- frountend/college-portal-frontend/src/api/index.js
+++ frountend/college-portal-frontend/src/api/index.js
@@ -1,9 +1,10 @@
 // Barrel file for API exports - helps with module resolution
-export * from './adminAPI';
-export * from './admissionAPI';
-export * from './authApi';
-export * from './contactAPI';
-export * from './leaveApi';
-export * from './staffApi';
-export * from './studentApi';
+// Note: Using .js extensions for Vercel/Linux compatibility
+export * from './adminAPI.js';
+export * from './admissionAPI.js';
+export * from './authApi.js';
+export * from './contactAPI.js';
+export * from './leaveApi.js';
+export * from './staffApi.js';
+export * from './studentApi.js';
 
